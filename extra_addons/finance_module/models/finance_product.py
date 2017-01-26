def359613fcaa11ae8c3a07977a411f8a89f0c1e
Edem <edememinov@hotmail.com>
2017-01-26 17:58:50 +0100

    Added changes to all models

--- extra_addons/finance_module/models/finance_product.py
+++ extra_addons/finance_module/models/finance_product.py
@@ -11,7 +11,7 @@ class FinanceProduct(models.Model):
     store = fields.Many2one('finance.shop', "Store")
     is_non_food = fields.Boolean('Product is non food')
     type_food = fields.Selection([('vegetables', 'Vegetables'), ('fruits', 'Fruits'), ('drinks','Drinks'), ('snacks','Snacks'), ('meat','Meat'), ('alcohol','Alcohol'), ('candy','Candy')])
-    type_non_food = fields.Selection([('hygene', 'Hygene'), ('phone', 'Phone Bill'), ('rent','Rent'), ('water','Water Bill'), ('gas','Gas Bill'), ('internet','Internet Bill'), ('electricity','Electricity Bill'), ('househole','Household objects'), ('electronics','Electronics'), ('games','Games')])
+    type_non_food = fields.Selection([('hygene', 'Hygene'), ('phone', 'Phone Bill'), ('rent','Rent'), ('water','Water Bill'), ('gas','Gas Bill'), ('internet','Internet Bill'), ('electricity','Electricity Bill'), ('househole','Household objects'), ('clothes','Clothes'), ('furniture','Furniture'), ('electronics','Electronics'), ('games','Games')])
 
 
 
